f82715e8aa399c9b4e716683ea26593f37711153
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-25 18:19:14 -0500

    wip: create route created

--- controllers/repo.js
+++ controllers/repo.js
@@ -8,7 +8,7 @@ const repoRouter = express.Router();
 // index route //
 repoRouter.get("/repo", (req, res) => {
   try {
-    res.json(await Repo.find(req.body));
+    res.json(await Repo.find({}));
   } catch (error) {
     res.status(400).json(error);
   }
@@ -18,6 +18,15 @@ repoRouter.get("/repo", (req, res) => {
 repoRouter.get("/repo/:id", async (req, res) => {
   try {
     res.json(await Repo.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+// create route //
+repoRouter.post("/", async (req, res) => {
+  try {
+    res.json(await Recipe.create(req.body));
   } catch (error) {
     res.status(400).json(error);
   }
