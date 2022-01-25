4161825f787ffd5e1a48c6cd481543a2ee3d73f2
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-25 18:32:05 -0500

    wip: schema/repoSchema created

--- models/repo.js
+++ models/repo.js
@@ -1,2 +1,15 @@
 // dependencies //
 const mongoose = require("mongoose");
+
+// schema //
+const Schema = mongoose.Schema;
+
+const repoSchema = new Schema(
+  {
+    author: String,
+    pName: String,
+    progress: Number,
+    url: String,
+  },
+  { timestamp: true }
+);
