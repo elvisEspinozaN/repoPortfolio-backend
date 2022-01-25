8798b58df80b3c66514b9b09fdd436bcf6795bc3
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-25 18:10:58 -0500

    wip: router/index router created

--- controllers/repo.js
+++ controllers/repo.js
@@ -1,3 +1,15 @@
 // dependencies //
 const express = require("express");
 const Repo = require("../models/repo");
+
+// router //
+const repoRouter = express.Router();
+
+// index route //
+repoRouter.get("/", (req, res) => {
+  try {
+    res.json(await Repo.find(req.body));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
