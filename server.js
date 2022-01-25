5272f0fd33d3db4d0a8f9f9bfc108e5bce34cbb1
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-01-25 17:59:35 -0500

    wip: connection set up in server.js

--- server.js
+++ server.js
@@ -9,3 +9,10 @@ const app = express();
 require("dotenv").config();
 
 const { PORT = 3001, DATABASE_URL } = process.env;
+
+// connection //
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+  .on("open", () => console.log("mongoDB connected"))
+  .on("close", () => console.log("mongoDB disconnected"))
+  .on("error", (error) => console.log(error));
